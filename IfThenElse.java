6a785825d273c2fe6db359dcf3843542d6a177d1
lekhanageetha1411-coder <lekhanageetha1411@gmail.com>
2025-10-26 10:31:21 -0700

    Create IfThenElse.java

---
+++ IfThenElse.java
@@ -0,0 +1,21 @@
+class ifthenelse{
+    public static void main(String[] args){
+        int testScore=50;
+
+        if(testScore>=90){
+            System.out.println("A");
+        }
+        else if(testScore>=80){
+            System.out.println("B");
+        }
+        else if(testScore>=70){
+            System.out.println("C");
+        }
+        else if(testScore>=60){
+            System.out.println("D");
+        }
+        else{
+            System.out.println("F");
+        }
+    }
+}
