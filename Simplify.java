4482e9d7ac936c0a7e67dc48cad2eb161ab30021
lekhanageetha1411-coder <lekhanageetha1411@gmail.com>
2025-11-04 06:35:21 -0800

    Create Simplify.java

---
+++ Simplify.java
@@ -0,0 +1,16 @@
+public class Simplify {
+  public static void main(String[] args) {
+
+    int a = 5;
+    int b = 7;
+
+    boolean exp1 = !(a == b && b >= a);
+    boolean exp2 = !(a==b) || !(b>=a);
+    boolean exp3 = a != b || b<a;
+
+   
+   
+   
+
+  }
+}
